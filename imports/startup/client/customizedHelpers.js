c4dfae19a4228947b6333299b541497948172fa1
Livia Peng <2356426722@qq.com>
2018-04-29 16:48:07 +0800

    修复pathFor bug

--- imports/startup/client/customizedHelpers.js
+++ imports/startup/client/customizedHelpers.js
@@ -1,8 +1,6 @@
 /**
  * Created by livia on 2018/1/20.
  */
-import {check} from 'meteor/check'
-import {Session} from 'meteor/session'
 import {Template} from 'meteor/templating'
 import {App} from '/imports/app.js'
 import {routerMeta} from '/imports/routerMeta.js'
